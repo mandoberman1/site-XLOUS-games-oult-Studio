4c196705f183a9f95bcef72bc5b22f886949a34e
Roman_doberman <manberezin13@gmail.com>
2024-10-20 21:48:58 +0700

    а

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let card = document.querySelectorAll('.card-game');
+let img = document.querySelectorAll('.card-game-img');
+let text = document.querySelectorAll('.hide-text');
+card.forEach((element, id) => {
+    element.addEventListener('mouseover', () => {
+        img[id].classList.add('card-game-img-hover')
+        text[id].classList.add('hide-text-hover')
+    })
+    element.addEventListener('mouseout', () => {
+        img[id].classList.remove('card-game-img-hover')
+        text[id].classList.remove('hide-text-hover')
+    })
+});
